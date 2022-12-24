bc36e16f17187ffea46542fb4acdb24060d4e7bc
darshil <chochadarshil2032004@gmail.com>
2022-12-24 13:54:02 +0530

    good

---
+++ 21ce047.java
@@ -0,0 +1,19 @@
+public class calc {
+    public static void main(String[] args) {
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        int e=Integer.parseInt(args[2]);
+ 
+        if(e==a)
+        {
+            int c= a+b;
+            System.out.println("Enter the sum of two numbers : ");
+        }
+        else if(e==b)
+        {
+            int d= a-b;
+            System.out.println("Substraction of two numbers : ");
+        }
+        else if(e==)
+    }
+}
